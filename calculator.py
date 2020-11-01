6f645d25f50291dd3c79a3d0f5c0d620de7c6539
Marek Starzyk <stachu.ms9@gmail.com>
2020-11-01 15:25:07 +0100

    Add checking mechanizm

--- calculator.py
+++ calculator.py
@@ -17,10 +17,33 @@ def div(a, b):
     return a / b
 
 
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 type_of_calculation = input("1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie\nJakie działanie chcesz wykonać?: ")
-number_a = float(input("Podaj pierwszą liczbę: "))
-number_b = float(input("Podaj drugą liczbę: "))
+
+while True:
+    number_a = input("Podaj pierwszą liczbę: ")
+    if is_number(number_a) is True:
+        number_a = float(number_a)
+        break
+    else:
+        logging.warning("Wpisana wartość nie jest liczbą, spróbuj jeszcze raz.")
+
+while True:
+    number_b = input("Podaj drugą liczbę: ")
+    if is_number(number_b) is True:
+        number_b = float(number_b)
+        break
+    else:
+        logging.warning("Wpisana wartość nie jest liczbą, spróbuj jeszcze raz.")
+
 if type_of_calculation == '1':
     logging.info("Dodaję %.2f do %.2f" % (number_a, number_b))
     print(f'Wynik dodawania: {add(number_a,number_b)}')
