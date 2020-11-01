474b637c5eb852c7e5a3f0a55a2846518f063674
Marek Starzyk <stachu.ms9@gmail.com>
2020-11-02 00:31:27 +0100

    Add new options in addition and multiplication

--- calculator.py
+++ calculator.py
@@ -1,20 +1,32 @@
 import logging
 
 
-def add(a, b):
-    return a + b
+def add(numbers_list):
+    _sum = 0
+    for number in numbers_list:
+        _sum += number
+    return _sum
 
 
-def sub(a, b):
-    return a - b
+def sub(number_list):
+    _sub = number_list[0]
+    for number in range(1, len(number_list)):
+        _sub -= number_list[number]
+    return _sub
 
 
-def mul(a, b):
-    return a * b
+def mul(number_list):
+    _mul = number_list[0]
+    for number in range(1, len(number_list)):
+        _mul *= number_list[number]
+    return _mul
 
 
-def div(a, b):
-    return a / b
+def div(number_list):
+    _div = number_list[0]
+    for number in range(1, len(number_list)):
+        _div /= number_list[number]
+    return _div
 
 
 def is_number(value):
@@ -26,18 +38,39 @@ def is_number(value):
 
 
 def is_sign_in_menu_correct(sign):
-    signs = ('1', '2', '3', '4', 't', 'n')
+    signs = ('1', '2', '3', '4', 'q')
     for item in signs:
         if sign.lower() == item:
             return True
 
 
+def num_lst(_range=2):
+    number_list = []
+    for number in range(1, _range + 1):
+        while True:
+            a = input("Podaj liczbę %d: " % number)
+            if is_number(a) is True:
+                a = float(a)
+                break
+            else:
+                logging.warning("\nWpisana wartość nie jest liczbą, spróbuj jeszcze raz.")
+        number_list.append(a)
+    return number_list
+
+
+def num_str(number_list, sign):
+    sentence = ''
+    for number in number_list:
+        sentence += str(number) + ' ' + str(sign) + ' '
+    sentence = sentence[:-2]
+    return sentence
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
+
 while True:
-    type_of_calculation = input("1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie\nJakie działanie chcesz wykonać?: ")
+    type_of_calculation = input("1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie, Q-Wyjście\nJakie działanie chcesz wykonać?: ")
     if is_sign_in_menu_correct(type_of_calculation) is True:
         break
     else:
@@ -45,15 +78,20 @@ while True:
 
 
 if type_of_calculation == '1':
-
-    logging.info("Dodaję %.2f do %.2f" % (number_a, number_b))
-    print(f'Wynik dodawania: {add(number_a,number_b)}')
+    _range = int(input("Ile liczb chcesz ze sobą dodać?: "))
+    numbers = num_lst(_range)
+    logging.info("Przeprowadzone działanie: " + num_str(numbers, '+'))
+    print(f'Wynik działania: {add(numbers)}')
 if type_of_calculation == '2':
-    logging.info("Odejmuję %.2f od %.2f" % (number_a, number_b))
-    print(f'Wynik odejmowania: {sub(number_a, number_b)}')
+    numbers = num_lst()
+    logging.info("Przeprowadzone działanie: " + num_str(numbers, '-'))
+    print(f'Wynik działania: {sub(numbers)}')
 if type_of_calculation == '3':
-    logging.info("Mnożę %.2f przez %.2f" % (number_a, number_b))
-    print(f'Wynik mnożenia: {mul(number_a, number_b)}')
+    _range = int(input("Ile liczb chcesz ze sobą pomnożyć?: "))
+    numbers = num_lst(_range)
+    logging.info("Przeprowadzone działanie: " + num_str(numbers, '*'))
+    print(f'Wynik działania: {mul(numbers)}')
 if type_of_calculation == '4':
-    logging.info("Dzielę %.2f przez %.2f" % (number_a, number_b))
-    print(f'Wynik mnożenia: {div(number_a, number_b)}')
+    numbers = num_lst()
+    logging.info("Przeprowadzone działanie: " + num_str(numbers, '/'))
+    print(f'Wynik działania: {div(numbers)}')
